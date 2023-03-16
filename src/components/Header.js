be06b55c95d6a04b9c8b2afaf7c2fd92ca017f1d
Marko2002Grujicic <m.grujicic2002@gmail.com>
2023-03-16 21:27:56 +0100

    completed feature 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,46 @@
 import './Header&Footer.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTh, faThList } from '@fortawesome/free-solid-svg-icons';
+
+import { useState, useEffect } from 'react';
+
 export const Header = () => {
-    return (
-        <header>
-            <h1>BIT People</h1>
-        </header>
-    )
-}
+  const [isGrid, setIsGrid] = useState(false);
+
+  useEffect(() => {
+    const container = document.querySelector('#container');
+    const userProfile = document.querySelectorAll('.userProfile');
+    const icon = document.querySelector('.view-icon');
+
+    if (isGrid) {
+      container.classList.add('grid-layout');
+      userProfile.forEach((element) => {
+        element.classList.add('grid-userProfile');
+      });
+      icon.classList.add('fas', 'fa-th');
+      icon.classList.remove('fa-th-list');
+    } else {
+      container.classList.remove('grid-layout');
+      userProfile.forEach((element) => {
+        element.classList.remove('grid-userProfile');
+      });
+      icon.classList.remove('fas', 'fa-th');
+      icon.classList.add('fa-th-list');
+    }
+  }, [isGrid]);
+
+  function handleClick() {
+    setIsGrid((prevIsGrid) => !prevIsGrid);
+  }
+
+  return (
+    <header>
+      <h1>BIT People</h1>
+      
+        <button onClick={handleClick} className="headerIcon">
+        <FontAwesomeIcon className="view-icon" icon={isGrid ? faTh : faThList} />
+        </button>
+  
+    </header>
+  );
+};
