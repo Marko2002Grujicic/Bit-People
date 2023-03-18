d3bcb8fa87c9f6f9239448e39e80655fb1512498
Marko2002Grujicic <m.grujicic2002@gmail.com>
2023-03-18 10:16:16 +0100

    feature 8 complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Header } from './components/Header';
 import './App.css';
 import { Users } from './components/Users';
-
+import { Footer } from './components/Footer';
 
 function App() {
   const handleReload = () => {
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <Header  handleReload={handleReload}></Header>
       <Users ></Users>
-      
+      <Footer/>
     </div>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faEnvelope, faCake} from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope, faCake, faSearch} from '@fortawesome/free-solid-svg-icons';
 import './Users.css'
+import './Search/Search.css'
 
 export const Users = () => {
     const [users,setUsers] = useState([]);
-
+    const [searchText, setSearchText] = useState('');
 
     useEffect(() => {
         fetch('https://randomuser.me/api/?results=15')
@@ -15,7 +16,6 @@ export const Users = () => {
         });
     }, []);
 
-
     const hideEmail = (email) => {
         const atIndex = email.indexOf("@");
         const username = email.slice(0, atIndex);
@@ -24,24 +24,39 @@ export const Users = () => {
         return hiddenUsername + domain
     }
     
-    
     users.forEach(user => {
         user.hiddenEmail = hideEmail(user.email);
         user.formatedUsername = `${user.name.first} ${user.name.last}`;
     });
 
-    const gender = (user) => {
+    const checkGender = (user) => {
         let className = ""
         if (user.gender === 'female'){
             className = 'female';
         }
         return className;
     }
+    const filteredUsers = users.filter((user) =>
+    user.name.first.toLowerCase().includes(searchText.toLowerCase()) || user.name.last.toLowerCase().includes(searchText.toLocaleLowerCase())
+  );
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
 
     return (
-        <div className="container" id="container">
-            {users.map(user => (
-                <div key={user.email} className={`userProfile ${gender(user)}`} id="userProfile">
+            <div className="container" id="container">
+             <div id="search-container">
+            <FontAwesomeIcon icon={faSearch} className="icon" />
+            <input
+             type="text"
+            placeholder="Search users"
+            id="search-input"
+            value={searchText}
+            onChange={handleSearch}
+             />
+             </div>
+            {filteredUsers.map(user => (
+                <div key={user.email} className={`userProfile ${checkGender(user)}`} id="userProfile">
                     <img src={user.picture.large} alt="user" className="image"/>
                         <div className="userInfo">
                             <span>Name: {user.formatedUsername}</span>
