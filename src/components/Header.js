6c23c8847ad5a2309c9aa3d02474ac24ab94005b
Marko2002Grujicic <m.grujicic2002@gmail.com>
2023-03-18 13:09:51 +0100

    feature 11 closing up

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,7 @@ import './Header&Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTh, faThList, faArrowRotateRight } from '@fortawesome/free-solid-svg-icons';
 
-import { useState } from 'react';
-
-export const Header = ({handleReload }) => {
-    const [isGrid, setIsGrid] = useState(false);
-
-    const handleClick = () => {
-      const container = document.querySelector('#container');
-      const userProfile = document.querySelectorAll('.userProfile');
-      const icon = document.querySelector('.view-icon');
-  
-      if (isGrid) {
-        container.classList.remove('grid-layout');
-        userProfile.forEach((profile) => profile.classList.remove('grid-userProfile'));
-        icon.classList.add('fa-th-list');
-        setIsGrid(false);
-        
-      } else {
-        container.classList.add('grid-layout');
-        userProfile.forEach((profile) => profile.classList.add('grid-userProfile'));
-        icon.classList.remove('fa-th-list');
-        setIsGrid(true);
-        
-      }
-    }
+export const Header = ({toggleLayout, layout ,handleReload }) => {
 
   return (
     <header>
@@ -34,13 +11,10 @@ export const Header = ({handleReload }) => {
         <button onClick={handleReload}>
         <FontAwesomeIcon icon={faArrowRotateRight} className="view-icon"/>
         </button>
-        <button onClick={handleClick}>
-        <FontAwesomeIcon className="view-icon" icon={isGrid ? faTh : faThList} />
+        <button onClick={toggleLayout}>
+        <FontAwesomeIcon className="view-icon" icon={layout === "list" ? faTh : faThList} />
         </button>
-        
       </div>
-        
-  
     </header>
   );
 };
---
+++ src/components/UserGrid.js
@@ -0,0 +1,48 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faEnvelope, faCake} from '@fortawesome/free-solid-svg-icons';
+import './UsersGrid.css'
+
+
+
+export const UsersGrid = ({users, }) => {
+
+    const hideEmail = (email) => {
+        const atIndex = email.indexOf("@");
+        const username = email.slice(0, atIndex);
+        const domain = email.slice(atIndex);
+        const hiddenUsername = username.slice(0,3) + "..." + username.slice(-3);
+        return hiddenUsername + domain
+    }
+    
+    users.forEach(user => {
+        user.hiddenEmail = hideEmail(user.email);
+        user.formatedUsername = `${user.name.first} ${user.name.last}`;
+    });
+
+    const checkGender = (user) => {
+        let className = ""
+        if (user.gender === 'female'){
+            className = 'female';
+        }
+        return className;
+    }
+
+    return (
+        <>
+                <div className="grid-layout" id="container">
+                {users.map(user => (
+                <div key={user.email} className={`${checkGender(user)} grid-userProfile `} id="userProfile">
+                    <img src={user.picture.large} alt="user" className="image"/>
+                        <div className="userInfo">
+                            <span>Name: {user.formatedUsername}</span>
+                            <span><FontAwesomeIcon icon={faEnvelope} className="icon bold"/>Email: {user.hiddenEmail}</span>
+                            <span><FontAwesomeIcon icon={faCake} className="icon bold"/> {new Date(user.dob.date).toLocaleDateString("en-GB")}</span>
+                        </div>
+                </div>
+                ))}
+            </div>
+        </>
+        
+        
+    )
+}
